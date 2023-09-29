f7ece69ce6fc36422e456382c999ff4878016272
Elena <elena.1415.23@gmail.com>
2023-09-29 11:03:53 +0700

    COMMIT

---
+++ app.vue
@@ -0,0 +1,86 @@
+<template>
+  <el-container class="layout-container-demo">
+    <el-header style="text-align: right; font-size: 12px">
+      <div class="toolbar">
+        <el-dropdown>
+          <el-icon style="margin-right: 8px; margin-top: 1px"
+            ><setting
+          /></el-icon>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item>View</el-dropdown-item>
+              <el-dropdown-item>Add</el-dropdown-item>
+              <el-dropdown-item>Delete</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+        <span>Tom</span>
+      </div>
+    </el-header>
+
+    <el-container>
+      <el-aside width="200px">
+        <el-scrollbar height="calc(100vh - 60px)">
+          <el-menu style="font-size: 12px" active-color="#689EFF">
+            <el-menu-item index="1">
+              <el-icon><Document /></el-icon>
+              <span>Ком.условие</span>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <el-icon><Suitcase /></el-icon>
+              <span>Поставщики</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <el-icon><Calendar /></el-icon>
+              <span>График расчетов</span>
+            </el-menu-item>
+          </el-menu>
+        </el-scrollbar>
+      </el-aside>
+      <el-main>
+        <NuxtLayout />
+        <NuxtPage />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script lang="ts" setup>
+//import { ref } from "vue";
+import { Calendar, Document, Suitcase, Setting } from "@element-plus/icons-vue";
+
+</script>
+
+<style scoped>
+.layout-container-demo .el-header {
+  position: relative;
+  color: var(--el-text-color-primary);
+  border-bottom: 1px solid var(--el-border-color);
+}
+.layout-container-demo .el-aside {
+  color: var(--el-text-color-primary);
+}
+.layout-container-demo .el-menu {
+  border-right: none;
+  font-size: 10px;
+}
+.layout-container-demo .el-menu title {
+  font-size: 10px;
+}
+.el-sub-menu__title {
+  font-size: 12px;
+}
+.layout-container-demo .el-main {
+  padding: 0;
+}
+.layout-container-demo .toolbar {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  right: 20px;
+}
+.content {
+  padding: 0;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { 
+    enabled: true 
+  },
+  app:{
+    head:{
+      title: 'Сайт'
+    }
+  },
+  css:[
+     '~/assets/css/main.css'
+  ],
+  modules: [
+    '@pinia/nuxt',
+    '@element-plus/nuxt',
+  ],
+  pinia: {
+    autoImports: [
+      // automatically imports `defineStore`
+      'defineStore', // import { defineStore } from 'pinia'
+      ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+    ],
+  },
+})
+
+
